a38bd13f73e543ffacd6838386adf5afd98b49ab
xsthunder <xsthunder@outlook.com>
2020-03-24 23:37:54 +0800

    complie from source

--- rebuild.ps1
+++ rebuild.ps1
@@ -4,8 +4,9 @@ $ErrorActionPreference = "Stop" # exit on error
 $version=get-content ./config/version.txt
 $project_name=get-content ./config/project_name.txt
 
-# pip install setuptools wheel # --user
+# pip install setuptools wheel twine # --user
 
 pip uninstall $project_name -y
 python setup.py sdist bdist_wheel
+pip install .\dist\$project_name-$version.tar.gz
 pip install .\dist\$project_name-$version-py3-none-any.whl
