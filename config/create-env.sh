976e7f102db0afb613d28eaa231bb4777e920256
xsthunder <xsthunder@outlook.com>
2020-03-24 17:54:54 +0800

    Update create-env.sh

--- config/create-env.sh
+++ config/create-env.sh
@@ -5,7 +5,12 @@ then
 fi
 
 # py3.7 for asyncio.WindowsProactorEventLoopPolicy() support
-conda create -n test IPython tqdm -c conda-forge -y
+
+env_name=test
+
+conda create -n $env_name IPython tqdm -c conda-forge -y
+
+conda activate $env_name
 yes | pip install sure
 
 # sure=1.4.11
