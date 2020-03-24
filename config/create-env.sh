28cccc20443e494b7b63b6d5e2995b50558f9487
xsthunder <xsthunder@outlook.com>
2020-03-24 18:23:46 +0800

    update set-env

--- config/create-env.sh
+++ config/create-env.sh
@@ -5,9 +5,16 @@ then
 fi
 
 # py3.7 for asyncio.WindowsProactorEventLoopPolicy() support
-conda create -n test IPython tqdm -c conda-forge -y
-yes | pip install sure
+
+env_name=test
+conda create -n $env_name IPython tqdm -c conda-forge -y
+
+conda activate $env_name
+yes | pip install sure xsthunder-python-lib
+
 
 # sure=1.4.11
 # ipython=7.8.0
 # tqdm=4.36.1
+# python=3.7.4
+# xsthunder-python-lib=0.0.1
